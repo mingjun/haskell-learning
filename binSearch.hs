af0e53ea48caab9470ae09e6bd4337f511020386
Mingjun Xu <xmj0236@gmail.com>
2013-06-12 22:58:13 +0800

    beautify binSearch

--- binSearch.hs
+++ binSearch.hs
@@ -2,12 +2,13 @@
 chop v [] = -1
 
 chop v [w] = case v==w of
-             True   ->  0
-             False  -> -1
+               True   ->  0
+               False  -> -1
 
 chop v list | v == w = m
             | v < w = chop v headHalf
-            | otherwise = (\x -> if x>=0 then m+1+x else x ) $ chop v tailHalf
+            | otherwise = m+1 `positiveAdd` chop v tailHalf
             where
-                  m = (length list - 1) `div` 2 ;
-                  (headHalf, (w:tailHalf)) = splitAt m list
+              m = (length list - 1) `div` 2 ;
+              (headHalf, (w:tailHalf)) = splitAt m list
+              positiveAdd x y = if x < 0 || y < 0 then -1 else x+y
