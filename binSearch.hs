f9b11541718d48307bdf91de14c227081c183de7
Mingjun Xu <xmj0236@gmail.com>
2013-06-12 18:46:11 +0800

    fix binary search

--- binSearch.hs
+++ binSearch.hs
@@ -5,14 +5,9 @@ chop v [w] = case v==w of
              True   ->  0
              False  -> -1
 
-chop v [x,y] | v == x = 0
-             | v == y = 1
-             | otherwise = -1
-
 chop v list | v == w = m
             | v < w = chop v headHalf
-            | otherwise = m + 1 + (chop v tailHalf)
-            where m = (length list - 1) `div` 2
+            | otherwise = (\x -> if x>=0 then m+1+x else x ) $ chop v tailHalf
+            where
+                  m = (length list - 1) `div` 2 ;
                   (headHalf, (w:tailHalf)) = splitAt m list
-
-
